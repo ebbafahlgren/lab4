a3a0cf0e3cc70dfe87865db2c3db380cde060db3
ebbafahlgren <79453176+ebbafahlgren@users.noreply.github.com>
2021-02-22 13:03:05 +0100

    Update GameGrid.java

--- lab4/src/lab4/data/GameGrid.java
+++ lab4/src/lab4/data/GameGrid.java
@@ -1,5 +1,68 @@
 package lab4.data;
 
-public class GameGrid {
+import java.util.Observable;
 
+/**
+ * Represents the 2-d game grid
+ */
+
+public class GameGrid extends Observable{
+
+	//The board 2d
+	//Size som parameter till konstruktorn
+	//Om size n är storleken har den n^2 rutor
+	
+	// 3 st public static integer
+	//EMPTY, ME, OTHER
+	
+	//INROW för att kolla om n i rad - konstant, börjar m 5
+	//horisontellt, vertikalt och lodrätt
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param size The width/height of the game grid
+	 */
+	public GameGrid(int size){}
+	
+	/**
+	 * Reads a location of the grid
+	 * 
+	 * @param x The x coordinate
+	 * @param y The y coordinate
+	 * @return the value of the specified location
+	 */
+	public int getLocation(int x, int y){}
+	
+	/**
+	 * Returns the size of the grid
+	 * 
+	 * @return the grid size
+	 */
+	public int getSize(){}
+	
+	/**
+	 * Enters a move in the game grid
+	 * 
+	 * @param x the x position
+	 * @param y the y position
+	 * @param player
+	 * @return true if the insertion worked, false otherwise
+	 */
+	public boolean move(int x, int y, int player){}
+	
+	/**
+	 * Clears the grid of pieces
+	 */
+	public void clearGrid(){}
+	
+	/**
+	 * Check if a player has 5 in row
+	 * 
+	 * @param player the player to check for
+	 * @return true if player has 5 in row, false otherwise
+	 */
+	public boolean isWinner(int player){}
+	
+	
 }
