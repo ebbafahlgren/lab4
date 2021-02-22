6769603e411ef8753e7050b29243d2dfd4b2dd3f
ebbafahlgren <79453176+ebbafahlgren@users.noreply.github.com>
2021-02-22 13:04:30 +0100

    Update GomokuMain.java

--- lab4/src/lab4/GomokuMain.java
+++ lab4/src/lab4/GomokuMain.java
@@ -1,5 +1,16 @@
 package lab4;
 
 public class GomokuMain {
+	//Argument av main - port number
+	
+	
+	//Skapa 3 objekt;
+	//GomokuClient - port number som arg
+	//GomokuGameState - client som arg
+	//GumokuGUI - game state & client som arg
+	
+	//Kod att kolla bara ett argument,
+	//Annars sätts port nummer till default, minst 4000
 
+	
 }
